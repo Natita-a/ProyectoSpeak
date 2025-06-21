2693167d39e326c12cb8fe31dc502c43bd158a99
Natita-a <carolito133@gmail.com>
2025-06-21 10:40:22 -0600

    Update views.py
    Importar el modelo y el archivo serializer.py

--- views.py
+++ views.py
@@ -2,7 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import Usuarios
+from .models import PreferenciasUsuarios
+from .serializers import PreferenciasUsuariosSerializer
 import random
 import string
 
