d42e6b40bf6bbb7aa9a9642fb899a24226a9c996
Natita-a <carolito133@gmail.com>
2025-07-08 13:35:11 -0600

    Add files via upload
    Para la practica propia

---
+++ src/pages/PracticaPropia.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useRef, useEffect } from "react";
+import { FaStopCircle, FaMicrophone } from "react-icons/fa";
+import { useLocation } from "react-router-dom";
+import '../styles/Recorder.css';
+import api from '../components/User';
+import Recorder from "../components/Recorder";
+
+
+function PracticaPropia() {
+  return (
+    <>
+      <Recorder />
+    </>
+  );
+}
+
+export default PracticaPropia;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
