874cd86b58c652c973c26c014a3eefb046c9e924
Natita-a <carolito133@gmail.com>
2025-07-21 16:23:26 -0600

    Update urls.py
    Se agregan los nuevos modos de exposicion y aleatorios

--- urls.py
+++ urls.py
@@ -21,9 +21,13 @@ from Proyecto.views import GuardarProposito
 from Proyecto.views import GuardarPreferencia
 from Proyecto.views import VerificarPreferencias
 from Proyecto.views import GenerarSituacionTemaPropio
+from Proyecto.views import GenerarSituacionTemaAleatorio#Nuevo
 from Proyecto.views import EscenarioPracticaPropia
+from Proyecto.views import EscenarioPracticaAleatoria#Nuevo
 from Proyecto.views import TranscripcionAudioView
 from Proyecto.views import GuardarTranscripcionView
+from Proyecto.views import GenerarSituacionModoDebate#Nuevo
+from Proyecto.views import GenerarSituacionModoExposicion#Nuevo
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -37,16 +41,19 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')), 
     path('api/registro/', RegistroUsuario.as_view(), name='registro_usuario'),
-     path('api/guardar-proposito/', GuardarProposito.as_view(), name='guardar-proposito'),
+    path('api/guardar-proposito/', GuardarProposito.as_view(), name='guardar-proposito'),
     path('api/guardar-temas/', GuardarPreferencia.as_view(), name='guardar-preferencias'),
     path('api/verificar-preferencias/', VerificarPreferencias.as_view(), name='verificar_preferencias'),
     path('api/generar-temas/', GenerarSituacionTemaPropio.as_view(), name='generar-temas'),
+    path('api/generar-tema-aleatorio/',GenerarSituacionTemaAleatorio.as_view(),name='generar-tema-aleatorio'),
     path('api/escena-propia/',EscenarioPracticaPropia.as_view(),name='escena-propia'),
+    path('api/escena-aleatoria/',EscenarioPracticaAleatoria.as_view(),name='escena-aleatoria'),
     path('api/transcripcion/', TranscripcionAudioView.as_view(), name='transcripcion'),
-    path('api/generar-temas/', GenerarSituacionTemaPropio.as_view(), name='generar-temas'),
+    path('api/guardar-transcripcion/',GuardarTranscripcionView.as_view(),name='guardar-transcripcion'),
+    path('api/generar-modo-debate/',GenerarSituacionModoDebate.as_view(),name='generar-modo-debate'),
+    path('api/generar-modo-exposicion/',GenerarSituacionModoExposicion.as_view(),name='generar-modo-exposicion'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 
 ]
-
