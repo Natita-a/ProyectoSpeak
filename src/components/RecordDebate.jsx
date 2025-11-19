7d35ecdd766a9551a9b7ad66360b0c55a6955001
Natita-a <carolito133@gmail.com>
2025-11-18 23:22:19 -0600

    Add files via upload

---
+++ src/components/RecordDebate.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useRef, useEffect } from "react";
+import { FaStopCircle, FaMicrophone } from "react-icons/fa";
+import { useLocation } from "react-router-dom";
+import '../styles/Recorder.css';
+import api from '../components/User';
+import Recorder from "../components/Recorder";
+
+
+function PracticaDebate() {
+  return (
+    <>
+    
+    </>
+  );
+}
+
+export default PracticaDebate;
+
+
+
